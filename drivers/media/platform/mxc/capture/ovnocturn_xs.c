b47f6a30cbe35f82b318e6677eb30f7b008f2648
CDuke-Janus <52255664+CDuke-Janus@users.noreply.github.com>
2019-07-12 09:57:12 -0400

    removed test

--- drivers/media/platform/mxc/capture/ovnocturn_xs.c
+++ drivers/media/platform/mxc/capture/ovnocturn_xs.c
@@ -894,7 +894,6 @@ static void __exit nocturn_clean(void)                        //  @ECA
 module_init(nocturn_init);                                    //  @ECA
 module_exit(nocturn_clean);                                   //  @ECA
 //module_i2c_driver(nocturn_i2c_driver);			
-//test comment, ignore
 
 
 MODULE_AUTHOR("JANUS Research Group, Inc.");
